794e67b4285e54c202bfef6364cbc41bd91a388e
Lucian Hanga <lucianhanga@gmx.net>
2025-04-03 09:16:24 +0200

    Comment out unused security rules for RDP and SSH in Terraform configuration

--- terraform/proxy-vm.tf
+++ terraform/proxy-vm.tf
@@ -3,17 +3,17 @@ resource "azurerm_network_security_group" "proxy-vm" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
-  security_rule {
-    name                       = "RDP"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  # security_rule {
+  #   name                       = "RDP"
+  #   priority                   = 300
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "3389"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 }
 
 
